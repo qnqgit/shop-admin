bb5e80b56838ba24356545661c15ca2d13d7e504
qnqgit <qiaonanqing@sina.com>
2019-03-22 17:15:30 +0800

    登陆验证

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="login-wrap">
     <div class="login-container">
-      <el-form label-position="top" label-width="80px" :model="loginForm">
-        <el-form-item label="用户名">
+      <el-form label-position="top" :rules="loginFormRules" label-width="80px" :model="loginForm" ref="loginFormEl">
+        <el-form-item label="用户名" prop="username">
           <el-input v-model="loginForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
+        <el-form-item label="密码" prop="password">
           <el-input v-model="loginForm.password"></el-input>
         </el-form-item>
         <el-form-item>
@@ -17,7 +17,7 @@
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 export default {
   name: 'Login',
   data () {
@@ -25,14 +25,40 @@ export default {
       loginForm: {
         username: '',
         password: ''
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
       }
     }
   },
   methods: {
     async onSubmit () {
       this.$refs.loginFormEl.validate(valid => {
-
+        if (valid) {
+          this.login()
+        } else {
+          return false
+        }
       })
+    },
+    async login () {
+      const resData = await axios.post('http://localhost:8888/api/private/v1/login', this.loginForm)
+      const { meta } = resData.data
+      if (meta.status === 200) {
+        this.$message({
+          message: '登陆成功',
+          type: 'success'
+        })
+        this.$router.replace('/')
+      } else {
+        console.log(resData)
+        this.$message.error(`登陆失败: ${meta.msg}`)
+      }
     }
   }
 }
